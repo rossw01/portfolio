c436316012ccc80ef342275f0f64e17bdda11d26
rossw01 <56947241+rossw01@users.noreply.github.com>
2022-10-14 11:14:21 +0100

    added about me section to home page #29

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,16 @@ const bioPanel = document.getElementById("panelOne");
 const projectsPanel = document.getElementById("panelTwo");
 
 function scrollToProjects() {
-  projectsPanel.scrollIntoView({
-    // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
-    behavior: "smooth",
-    block: "start",
-  });
+	projectsPanel.scrollIntoView({
+		// https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+		behavior: "smooth",
+		block: "start",
+	});
+}
+
+function scrollToBio() {
+	bioPanel.scrollIntoView({
+		behavior: "smooth",
+		block: "start",
+	});
 }
